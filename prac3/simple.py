ba47bb8ffe1a44457639e7b9471b02f306d17a6e
Ilya Volkov <ilyan.wolf@gmail.com>
2023-09-07 11:54:17 +0300

    update

--- prac3/simple.py
+++ prac3/simple.py
@@ -5,7 +5,9 @@ def guess(n):
             if i*i== n:
                 return i
             else:
-                i = i +1
+                i = i + 1
+            if i not in range(1, n+1):
+                return "ne mogu tak"
 
 a=guess(n)
 print(a)
